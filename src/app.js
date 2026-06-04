2a15491a30be88b8ae3f085d700513f3b8d6795e
Canpolat Oral <canpoalotral@gmail.com>
2026-06-05 00:59:22 +0400

    Update log message to include a suffix in server startup

--- src/app.js
+++ src/app.js
@@ -15,5 +15,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(port, () => {
-  console.log(`hello-world listening on port ${port}`);
+  console.log(`hello-world listening on port ${port} 1`);
 });
