84dde51ce5aa067b49b69783d4f4751149d1462d
Canpolat Oral <canpoalotral@gmail.com>
2026-06-05 01:23:21 +0400

    Update server startup log message to reflect correct port number

--- src/app.js
+++ src/app.js
@@ -15,5 +15,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(port, () => {
-  console.log(`hello-world listening on port ${port} 2`);
+  console.log(`hello-world listening on port ${port} 3`);
 });
